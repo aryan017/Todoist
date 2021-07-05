c503a5518322ca1879768d040406f9f57c40144e
aryan017 <aryankumar2835@gmail.com>
2021-07-05 12:58:21 +0530

    Update Index.js

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -5,6 +5,7 @@ import { collatedTasksExist } from '../helpers';
 
 export const useTasks = selectedProject => {
     const [tasks,setTasks] = useState([]);
+    const [archivedTasks,setArchivedTasks]= useState([]);
 
     useEffect(() => {
        let unsubscribe = firebase
@@ -29,6 +30,45 @@ export const useTasks = selectedProject => {
                id: task.id,
                ...task.data(),
            }));
+     
+           setTasks(
+               selectedProject === "NEXT_7"
+               ? newTasks.filter(
+                   task => moment(task.date,"DD-MM-YYYY").diff(moment(), 'days') <= 7
+                   &&
+                   task.archived !==true
+               )
+               : newTasks.filter(task => task.archived !== true)
+           );
+
+          setArchivedTasks(newTasks.filter(task => task.archived !== false))
        });
-    }, [selectedProject])
-} 
+
+       return () => unsubscribe();
+    }, [selectedProject]);
+
+    return {tasks,archivedTasks};
+};
+
+export  const  useProjects = () => {
+    const [projects,setProjects] = useState([]);
+
+    useEffect(() => {
+        firebase
+        .firestore()
+        .collection('projects')
+        .where('userId', '==' , 'aMUYE893GFtyEcdvGO')
+        .orderBy('projectId')
+        .get()
+        .then(snapshot => {
+            const allProjects = snapshot.docs.map(project => ({
+                ...project.data(),
+                docId:project.Id,
+            }))
+            if(JSON.stringify(allProjects) !== JSON.stringify(projects)){
+                setProjects(allProjects);
+            }
+        })
+    },[projects]);
+    return {projects,setProjects};
+};
